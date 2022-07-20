bb0fe74923b00f7b33c5c5f51655320c62bc15b3
Theophilus Gordon <theophilusgordon@yahoo.com>
2022-07-20 16:06:39 +0000

    reverted to original code after mongo network access reset

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connector = await mongoose.connect(
-      "mongodb+srv://microfocusinc:incfocusmicro@access-key-manager.bbxzh.mongodb.net/?retryWrites=true&w=majority"
-    );
+    const connector = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${connector.connection.host}`);
   } catch (error) {
     console.log(error);
