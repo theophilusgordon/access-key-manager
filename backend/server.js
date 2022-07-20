df56dca337d4f2a0c2c86e84f89d35539cae4300
Theophilus Gordon <theophilusgordon@yahoo.com>
2022-07-20 10:46:11 +0000

    Prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const port = process.env.PORT || 5000;
 const errorHandler = require("./middleware/errorMiddleware");
@@ -15,6 +16,21 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/keys", require("./routes/accessKeyRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) =>
+    res.send("You should be in production environment to run this app")
+  );
+}
 
 app.use(errorHandler);
 
