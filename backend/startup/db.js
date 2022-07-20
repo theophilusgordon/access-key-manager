230e187c95eaf07bbe6c4ddda23854c572240ff0
Theophilus Gordon <theophilusgordon@yahoo.com>
2022-07-20 13:52:26 +0000

    reverted to original code

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  let url = `mongodb+srv://microfocusinc:incfocusmicro@access-key-manager.bbxzh.mongodb.net/?retryWrites=true&w=majority`;
-
   try {
-    const connector = await mongoose.connect(url);
+    const connector = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${connector.connection.host}`);
   } catch (error) {
     console.log(error);
